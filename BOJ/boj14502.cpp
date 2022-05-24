955d51691230be7b04492137a83e22fe23e86374
Choi Jun Ho <cjh970422@gmail.com>
2022-05-24 15:34:40 +0900

    boj14502

--- BOJ/boj14502.cpp
+++ BOJ/boj14502.cpp
@@ -1,45 +1,58 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
-int N, M;
 using namespace std;
 #define MAXSIZE 8
 int ans = 0;
+int N, M;
 int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
-bool visited[MAXSIZE][MAXSIZE];
 int table[MAXSIZE][MAXSIZE];
-int temp[MAXSIZE][MAXSIZE];
-
-void BFS(){
-	queue<pair<int,int>> que;
-	// virus
+int spread[MAXSIZE][MAXSIZE];
+int notSpread[MAXSIZE][MAXSIZE];
+void print(){
 	for(int n = 0; n < N; n++){
 		for(int m = 0; m < M; m++){
-			if(temp[n][m] == 2)
+			cout << spread[n][m];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void BFS(){
+    for(int n = 0; n < N; n++){
+		for(int m = 0; m < M; m++){
+		    spread[n][m] = notSpread[n][m];
+		}
+	}
+	
+	queue<pair<int,int>> que;
+	// virus spread
+	for(int n = 0; n < N; n++){
+		for(int m = 0; m < M; m++){
+			if(spread[n][m] == 2)
 				que.push(make_pair(m,n));
 		}
 	}
 	while(!que.empty()){
 		int tx = que.front().first;
 		int ty = que.front().second;
+		que.pop();
 		for(int n = 0; n < 4; n++){
 			int x = tx + dx[n];
 			int y = ty + dy[n];
 			if(x < 0 || y < 0 || x >= M || y >= N) continue;
-			if(visited[y][x] || table[y][x] == 1 || table[y][x] == 2) continue;
-			visited[y][x] = true;
-			temp[y][x] = 2;
+			if(spread[y][x] == 1 || spread[y][x] == 2) continue;
+			spread[y][x] = 2;
 			que.push(make_pair(x,y));
 		}
-		que.pop();
 	}
 }
 int safeZone(){
 	int cnt = 0;
 	for(int n = 0; n < N; n++){
 		for(int m = 0; m < M; m++){
-			if(temp[n][m] == 0)
+			if(spread[n][m] == 0)
 				cnt++;
 		}
 	}
@@ -49,21 +62,15 @@ void wall(int cnt){
 	if(cnt == 3){
 		BFS();
 		ans = max(ans, safeZone());
-		// default
-		for(int n = 0; n < N; n++){
-			for(int m = 0; m < M; m++){
-				temp[n][m] = table[n][m];
-				visited[n][m] = false;
-			}
-		}
 		return;
 	}
 	for(int n = 0; n < N; n++){
 		for(int m = 0; m < M; m++){
-			if(temp[n][m] == 0){
-				temp[n][m] == 1;
+			if(notSpread[n][m] == 0){
+				notSpread[n][m] = 1;
+				// print();
 				wall(cnt + 1);
-				temp[n][m] == 0;
+				notSpread[n][m] = 0;
 			}
 		}
 	}
@@ -76,12 +83,10 @@ int main() {
 	for(int n = 0; n < N; n++){
 		for(int m = 0; m < M; m++){
 			cin >> table[n][m];
+			notSpread[n][m] = table[n][m];
 		}
 	}
 	wall(0);
 	cout << ans << endl;
     return 0;
 }
-
-// ctrl + alt + c -> build
-// ctrl + alt + r -> execute\
