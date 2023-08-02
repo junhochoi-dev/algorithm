74a98d7ce0f42027e63e364307a362504dad9ac7
junhochoi-dev <cjh970422@gmail.com>
2023-08-02 13:37:50 +0900

    [solved] boj15650.java

--- BOJ/15650/boj15650.java
+++ BOJ/15650/boj15650.java
@@ -2,18 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
     static int N, M;
+    static int[] seq;
     static boolean[] visited = new boolean[8 + 1];
-    static void backtracking(int curr, int cnt, String ans){
+    static void backtracking(int curr, int cnt){
         if(cnt == M){
-            System.out.println(ans);
+            for(int i = 1; i <= M; i++){
+                sb.append(seq[i]).append(" ");
+            }
+            sb.append("\n");
             return;
         }
         for(int i = curr; i <= N; i++){
             if(curr == i) continue;
             if(visited[i]) continue;
             visited[i] = true;
-            backtracking(i, cnt + 1, ans + i + " ");
+            seq[cnt + 1] = i;
+            backtracking(i, cnt + 1);
             visited[i] = false;
         }
     }
@@ -22,12 +28,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        seq = new int[N + 1];
         M = Integer.parseInt(st.nextToken());
         for(int i = 1; i <= N; i++){
             visited[i] = true;
-            backtracking(i, 1, i + " ");
+            seq[1] = i;
+            backtracking(i, 1);
             visited[i] = false;
         }
+        System.out.println(sb);
     }
 }
 
