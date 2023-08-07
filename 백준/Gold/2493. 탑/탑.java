df0533f2cade3e53bb8d0eaca28fc8ac3650d99f
Choi Jun Ho <cjh970422@gmail.com>
2023-08-07 13:26:15 +0900

    [Gold V] Title: 탑, Time: 3140 ms, Memory: 126928 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        int[] ans = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Stack<Integer> stack = new Stack<Integer>();
+        int idx = 1;
+        while(true){
+            if(idx > N) break;
+            if(stack.empty()){
+                ans[idx] = 0;
+                stack.push(idx);
+                idx++;
+                continue;
+            }
+            if(arr[stack.peek()] < arr[idx]) {
+                stack.pop();
+            }
+            else{
+                ans[idx] = stack.peek();
+                stack.push(idx);
+                idx++;
+            }
+        }
+        for(int i = 1; i <= N; i++) System.out.print(ans[i] + " ");
+    }
+}
