ea08e5afd341a2898c32d5bb2eaa1162d27b6f42
Choi Jun Ho <cjh970422@gmail.com>
2024-02-28 09:20:46 +0900

    [Gold III] Title: 커넥티드 카 실험, Time: 1292 ms, Memory: 270680 KB -BaekjoonHub

--- 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.java
+++ 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.java
@@ -31,28 +31,22 @@ public class Main {
 
         Queue<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[N];
-        int MNLOC = arr.get(S).loc, MXLOC = arr.get(S).loc;
-        int MNIDX = S, MXIDX = S;
-        queue.add(S);
-        visited[S] = true;
+        int MNLOC = arr.get(S).loc, MXLOC = arr.get(S).loc, MNIDX = S, MXIDX = S;
+        queue.add(S); visited[S] = true;
         while (!queue.isEmpty()) {
             int curr = queue.poll();
             MNLOC = Math.min(MNLOC, arr.get(curr).loc - arr.get(curr).gas);
             MXLOC = Math.max(MXLOC, arr.get(curr).loc + arr.get(curr).gas);
-            // left
             for (int left = MNIDX - 1; left >= 0; left--) {
                 if (arr.get(left).loc < MNLOC) break;
                 if (visited[left]) continue;
-
                 MNIDX = Math.min(MNIDX, left);
                 visited[left] = true;
                 queue.add(left);
             }
-            // right
             for (int right = MXIDX + 1; right < N; right++) {
                 if (arr.get(right).loc > MXLOC) break;
                 if (visited[right]) continue;
-
                 MXIDX = Math.max(MXIDX, right);
                 visited[right] = true;
                 queue.add(right);
