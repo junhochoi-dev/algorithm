3fad8cdc11e27f88a22c305227e3e46c833892f0
Choi Jun Ho <cjh970422@gmail.com>
2024-03-14 23:34:53 +0900

    [Bronze III] Title: 差 (Difference), Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/31617. 差 （Difference）/README.md
@@ -0,0 +1,44 @@
+# [Bronze III] 差 (Difference) - 31617 
+
+[문제 링크](https://www.acmicpc.net/problem/31617) 
+
+### 성능 요약
+
+메모리: 14184 KB, 시간: 124 ms
+
+### 분류
+
+사칙연산, 브루트포스 알고리즘, 구현, 수학
+
+### 제출 일자
+
+2024년 3월 14일 23:34:38
+
+### 문제 설명
+
+<p>整数 <var>K</var> と，長さ <var>N</var> の整数列 <var>A=(A<sub>1</sub>,A<sub>2</sub>,…,A<sub>N</sub>)</var> および長さ <var>M</var> の整数列 <var>B=(B<sub>1</sub>,B<sub>2</sub>,…,B<sub>M</sub>)</var> が与えられる．</p>
+
+<p>次の条件をすべて満たす <var>2</var> つの整数の組 <var>(p,q)</var> の個数を求めよ．</p>
+
+<ul>
+	<li><var>1 ≦ p ≦ N</var>．</li>
+	<li><var>1 ≦ q ≦ M</var>．</li>
+	<li><var>A<sub>p</sub> + K = B<sub>q</sub></var>．</li>
+</ul>
+
+### 입력 
+
+ <p>入力は以下の形式で与えられる．</p>
+
+<pre><var>K</var>
+<var>N</var>
+<var>A<sub>1</sub></var>   <var>A<sub>2</sub></var>   <var>…</var>   <var>A<sub>N</sub></var>
+<var>M</var>
+<var>B<sub>1</sub></var>   <var>B<sub>2</sub></var> <var>…</var>   <var>B<sub>M</sub></var></pre>
+
+### 출력 
+
+ <p>条件をすべて満たす <var>2</var> つの整数の組 <var>(p,q)</var> の個数を出力せよ．</p>
+
+<p>答え以外は何も出力しないこと．(入力を促す文章なども出力しないこと．)</p>
+
---
+++ 백준/Bronze/31617. 差 （Difference）/差 （Difference）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int answer = 0;
+        int K = Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        int[] B = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (B[j] - A[i] == K) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
