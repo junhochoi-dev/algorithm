94a9f03129bb03c18a634f63a9b7e4983e6a8788
Choi Junho <cjh970422@gmail.com>
2024-05-18 20:10:37 +0900

    [Silver III] Title: 등수 매기기, Time: 860 ms, Memory: 54372 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		long answer = 0;
+		for (int i = 0; i < N; i++) {
+			answer += Math.abs(i + 1 - arr[i]);
+		}
+		System.out.println(answer);
+	}
+}
