b3db159f1bc0789c60575a0d917fbcfb4b1541a2
junhochoi-dev <cjh970422@gmail.com>
2024-05-22 09:16:26 +0900

    boj11568.java

---
+++ [PS]BOJ/10000_19999/11000_11999/11568/boj11568.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = 1;
+		}
+		int answer = 1;
+		for (int i = 1; i < N; i++) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			answer = Math.max(answer, dp[i]);
+		}
+		System.out.println(answer);
+	}
+}
