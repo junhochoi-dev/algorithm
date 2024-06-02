acee0d0c6ab9cb452edace39ec02342dfc4c37ae
Choi Junho <cjh970422@gmail.com>
2024-06-03 02:03:23 +0900

    [Bronze III] Title: 수박 게임, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/31868. 수박 게임/수박 게임.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		System.out.println(K / (1 << (N - 1)));
+	}
+}
