5ec0cea6d31494266c7b87b09df655cf0be80080
Choi Junho <cjh970422@gmail.com>
2024-06-19 16:14:34 +0900

    [Silver II] Title: 이진수 격자, Time: 112 ms, Memory: 14108 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/31910. 이진수 격자/이진수 격자.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		int[][] dp = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				dp[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				int u = i - 1 >= 0 ? dp[i - 1][j] : 0;
+				int l = j - 1 >= 0 ? dp[i][j - 1] : 0;
+				dp[i][j] = (Math.max(u, l) << 1) + dp[i][j];
+			}
+		}
+		System.out.println(dp[N - 1][N - 1]);
+	}
+}
