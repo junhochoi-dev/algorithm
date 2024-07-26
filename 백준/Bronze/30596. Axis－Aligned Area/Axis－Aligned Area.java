7ed580b2e5f5f87017392e646cc7084837187bec
Choi Junho <cjh970422@gmail.com>
2024-07-27 02:28:57 +0900

    [Bronze III] Title: Axis-Aligned Area, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/30596. Axis－Aligned Area/Axis－Aligned Area.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        int A1 = Integer.parseInt(br.readLine());
+        int A2 = Integer.parseInt(br.readLine());
+        int A3 = Integer.parseInt(br.readLine());
+        int A4 = Integer.parseInt(br.readLine());
+        System.out.println(A1 * A3);
+    }
+}
