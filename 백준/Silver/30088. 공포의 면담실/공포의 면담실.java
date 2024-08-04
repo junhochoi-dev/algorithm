7d024a55d055222f2c76366d49a2a7307aeb7bf1
Choi Junho <cjh970422@gmail.com>
2024-08-05 00:17:51 +0900

    [Silver V] Title: 공포의 면담실, Time: 552 ms, Memory: 84312 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -7,17 +7,17 @@ public class Main {
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		long[] arr = new long[N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int sum = 0, size = Integer.parseInt(st.nextToken());;
+			int sum = 0, size = Integer.parseInt(st.nextToken());
 			for (int j = 0; j < size; j++) {
 				sum += Integer.parseInt(st.nextToken());
 			}
 			arr[i] = sum;
 		}
 		Arrays.sort(arr);
-		int answer = 0;
+		long answer = 0;
 		for (int i = 0; i < N; i++) {
 			answer += arr[i] * (N - i);
 		}
