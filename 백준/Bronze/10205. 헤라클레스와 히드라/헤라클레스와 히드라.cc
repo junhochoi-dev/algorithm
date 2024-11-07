69478b69e9a268054e925b6502e5582f2bf1a0e5
Choi Junho <cjh970422@gmail.com>
2024-11-08 00:22:35 +0900

    [Bronze III] Title: 헤라클레스와 히드라, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10205. 헤라클레스와 히드라/헤라클레스와 히드라.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define loop(k, sz) for(int k = 0; k < sz; k++)
+#define ll long long
+#define endl '\n'
+
+int main() {
+    fastio
+    int K; cin >> K;
+    loop(k, K){
+        int H; cin >> H;
+        string act; cin >> act;
+        loop(i, act.size()){
+            if(act[i] == 'c') H++;
+            else H--;
+        }
+        cout << "Data Set " << k + 1 << ":" << endl << H << endl << endl;
+    }
+    return 0;
+}
+
