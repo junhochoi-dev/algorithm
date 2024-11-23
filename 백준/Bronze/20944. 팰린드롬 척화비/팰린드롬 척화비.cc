a617652b876cc1d92283547915f7936891a0d6a6
Choi Junho <cjh970422@gmail.com>
2024-11-24 00:47:33 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define loop(k, sz) for(int k = 0; k < sz; k++)
+#define ll long long
+#define endl '\n'
+
+int main() {
+    fastio
+    int N; cin >> N;
+    while(N--) cout << "a";
+    return 0;
+}
+
